85e3ea2a94b30c81f2061cb2fdcb1f8c98723df5
Ajmir Aribam <ajmiraribam@Ajmirs-MacBook-Air.local>
2026-04-24 20:56:03 +0530

    3Sum using Sorting and Two Pointer

---
+++ day03_arrays/3_Sum_15.py
@@ -0,0 +1,37 @@
+class Solution:
+    def threeSum(self, A):
+        A.sort()
+        result = []
+
+        for i in range(len(A)):
+            if i > 0 and A[i] == A[i-1]:
+                continue
+            left = i+1
+            right = len(A) - 1
+            
+            while left < right:
+                total = A[i] + A[left] + A[right]
+                if total == 0:
+                    result.append([A[i], A[left], A[right]])
+                    left += 1
+                    right -= 1
+
+                    while left < right and A[left] == A[left-1]:
+                        left += 1
+                    while left < right and A[right] == A[right+1]:
+                        right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+        
+        return result
+
+if __name__ == "__main__":
+    A = [-1,0,1,2,-1,-4]
+    sol = Solution()
+    result = sol.threeSum(A)
+    print("Input:", A)
+    print("3Sum Result:", result)
+
+        
