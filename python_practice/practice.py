e24f62867c8700eada0a534989ad697f4b44ca99
Ajmir Aribam <ajmiraribam@Ajmirs-MacBook-Air.local>
2026-04-27 14:18:13 +0530

    Spiral Matrix

--- python_practice/practice.py
+++ python_practice/practice.py
@@ -1,9 +1,17 @@
 class Solution:
-    def groupAnalgrams(self, strs):
-        analgram_map = {}
-        for word in strs:
-            key = "".join(sorted(word))
-            if key not in analgram_map:
-                analgram_map[key] = []
-            analgram_map[key].append(word)
-        return list(analgram_map.values())
+    def rotate(self, matrix):
+        n = len(matrix)
+        for i in range(n): # Transpose(swapping across diagonal)
+            for j in range(i, n): # To avoid double swapping
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        for i in range(n): # reverse each row
+            matrix[i].reverse()
+
+if __name__ == "__main__":
+    matrix = [[1,2,3], [4,5,6], [7,8,9]]
+    sol = Solution()
+    ans = sol.rotate(matrix)
+    print("After rotatation of matrix: ")
+    for row in matrix:
+        print(row)
