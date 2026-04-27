9484a85da177f8ce6131bc9b9fd7de8f625fe488
Ajmir Aribam <ajmiraribam@Ajmirs-MacBook-Air.local>
2026-04-27 11:18:54 +0530

    Rotate Matrix 90° Clockwise using Transpose + Reverse

---
+++ day06_matrix/rotate_image_48.py
@@ -0,0 +1,18 @@
+class Solution:
+    def rotate(self, matrix):
+        n = len(matrix)
+        
+        for i in range(n): # Transpose matrix (swap across diagonal)
+            for j in range(i,n): # To avoid double swapping
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        
+        for i in range(n): # Reverse each row
+            matrix[i].reverse()
+
+if __name__ == "__main__":
+    matrix = [[1,2,3], [4,5,6], [7,8,9]]
+    sol = Solution()
+    ans = sol.rotate(matrix)
+    print("After rotatation of matrix: ")
+    for row in matrix:
+        print(row)
