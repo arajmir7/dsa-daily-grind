ec8ec1d7868ee3bd83c8a02b6d4051e2a8443d04
Ajmir Aribam <ajmiraribam@Ajmirs-MacBook-Air.local>
2026-05-06 22:51:35 +0530

    Binary Tree Zigzag Level Order Traversal using BFS

---
+++ day15_tree_bfs/bt_zigzag_level_order_traversal_103.py
@@ -0,0 +1,50 @@
+from collections import deque
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def zigzagLevelOrder(self, root):
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+        leftToRight = True
+
+        while queue:
+            level = []
+            size = len(queue)
+
+            for _ in range(size):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+            if not leftToRight:
+                level.reverse()
+
+            result.append(level)
+            leftToRight = not leftToRight
+
+        return result
+    
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
+    sol = Solution()
+    result = sol.zigzagLevelOrder(root)
+    print("Zigzag Level Order Traversal:")
+    print(result)
